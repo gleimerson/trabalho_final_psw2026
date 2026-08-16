9340236460d7b14c4a2511790bfa1463cc1f123a
gleimerson <146475346+gleimerson@users.noreply.github.com>
2026-08-16 14:51:17 -0300

    .

--- Lanchonete_Modelo/produtos/models.py
+++ Lanchonete_Modelo/produtos/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Pessoa(models.Model):
+    nome = models.CharField(max_length=100)
+    cpf = models.CharField(max_length=14)
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=255)
+
+    
