337da327c038b3ddc0ec191a9512b8160a882fba
gleimerson <146475346+gleimerson@users.noreply.github.com>
2026-08-16 21:42:50 -0300

    .

--- Lanchonete_Modelo/produtos/admin.py
+++ Lanchonete_Modelo/produtos/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Categoria, Pessoa
+from .models import Categoria, Pessoa, Produto, Pedido, PedidoProduto
 
 # Register your models here.
 admin.site.register(Categoria)
 admin.site.register(Pessoa)
+admin.site.register(Produto)
+admin.site.register(Pedido)
+admin.site.register(PedidoProduto)
--- Lanchonete_Modelo/produtos/models.py
+++ Lanchonete_Modelo/produtos/models.py
@@ -1,12 +1,50 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Pessoa(models.Model):
     nome = models.CharField(max_length=100)
     cpf = models.CharField(max_length=14)
 
+    usuario = models.OneToOneField(
+        User, 
+        on_delete=models.CASCADE
+    )
+
 class Categoria(models.Model):
     nome = models.CharField(max_length=100)
     descricao = models.CharField(max_length=255)
 
-    
+class Produto(models.Model):
+    nome = models.CharField(max_length=100)
+    preco = models.FloatField()
+    descricao = models.CharField(max_length=255)
+    disponivel = models.BooleanField(default=True)
+    imagem = models.ImageField(upload_to='produtos/', blank=True, null=True)
+
+    categoria = models.ForeignKey(
+        Categoria,
+        on_delete=models.CASCADE
+    )
+class Pedido(models.Model):
+    data_pedido = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=50)
+    valor_total = models.FloatField()
+
+    pessoa = models.ForeignKey(
+        Pessoa,
+        on_delete=models.CASCADE
+    )
+
+class PedidoProduto(models.Model):
+    pedido = models.ForeignKey(
+        Pedido,
+        on_delete=models.CASCADE
+    )
+
+    produto = models.ForeignKey(
+        Produto,
+        on_delete=models.CASCADE
+    )
+
+    preco_unitario = models.FloatField()
+    quantidade = models.IntegerField()
