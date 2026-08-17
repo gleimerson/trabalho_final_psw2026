41a44d0ef36c725600a8ac07e9e33184f53d2c1f
gleimerson <146475346+gleimerson@users.noreply.github.com>
2026-08-17 19:30:39 -0300

    .

--- Lanchonete_Modelo/produtos/admin.py
+++ Lanchonete_Modelo/produtos/admin.py
@@ -2,8 +2,4 @@ from django.contrib import admin
 from .models import Categoria, Pessoa, Produto, Pedido, PedidoProduto
 
 # Register your models here.
-admin.site.register(Categoria)
-admin.site.register(Pessoa)
-admin.site.register(Produto)
-admin.site.register(Pedido)
-admin.site.register(PedidoProduto)
+
